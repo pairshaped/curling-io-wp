14529dce1edc8953c2ba372db07f041359d44175
Alex <ulkoalex@gmail.com>
2014-08-13 02:06:28 +0300

    version 1.0.1

--- curlcast.php
+++ curlcast.php
@@ -11,9 +11,7 @@ if(!class_exists('curlcast'))
 
 	define('WP_CURLCAST_PAGE_URL', 'http://tothebutton.com/stats');
 	define('WP_CURLCAST_WIDGET_URL', 'http://widgets.tothebutton.com/stats/scoreboard_mini.html');
-
-	define('WP_CURLCAST_URL_CHECK', 'http://curlcast.ulko.net/check.php');
-	define('WP_CURLCAST_URL_UPDATE', 'http://curlcast.ulko.net/update.php');
+	define('WP_CURLCAST_UPDATE_URL', 'http://curlcast.ulko.net/update.php');
 
 	defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 
@@ -51,7 +49,7 @@ if(!class_exists('curlcast'))
 							),
 						'curlcast_page_id' => array (
 							'name' 		=> __('Page', 'curlcast'),
-							'helptext'	=> __('Page used for plugin routing', 'curlcast'),
+							'helptext'	=> __('Page used for plugin output', 'curlcast'),
 							'default'	=> '',
 							'required'	=> 'yes',
 							'type' 		=> 'dropdown_pages'
@@ -97,7 +95,7 @@ if(!class_exists('curlcast'))
 				add_action('init', array('curlcast', 'main'), 1);
 				add_filter('query_vars', array('curlcast', 'query_vars'), 10);
 				add_action('admin_notices', array('curlcast', 'admin_notice'));
-				add_action('admin_notices', array('curlcast', 'check_for_new_version'));
+				//add_action('admin_notices', array('curlcast', 'check_for_new_version'));
 				add_action('template_redirect', array('curlcast', 'template_redirect'), 11);
 				add_action('wp_loaded', array('curlcast', 'flush_rules'));
 				add_action('widgets_init', array('curlcast', 'load_widget'));
@@ -177,6 +175,7 @@ if(!class_exists('curlcast'))
 			{
 			$page_prefix = get_option('curlcast_page_prefix');
 			add_rewrite_rule($page_prefix.'/(.*)$', 'index.php?curlcast_page=$matches[1]', 'top');
+			self::auto_update();
 			}
 
 		/**
@@ -334,11 +333,11 @@ if(!class_exists('curlcast'))
 				{
 					case 'checkbox':
 						if($value == 'yes')  $checked = 'checked="checked"'; else $checked = '';
-						$edit = '<p style="margin: 5px"><input id="'.htmlspecialchars($option).'" type="checkbox" name="'.htmlspecialchars($option).'" value="yes" '.$checked.'/>&nbsp;&nbsp;&nbsp;<i style="color:#aaa">'.$help.'</i></p>';
+						$edit = '<p style="margin: 2px"><input id="'.htmlspecialchars($option).'" type="checkbox" name="'.htmlspecialchars($option).'" value="yes" '.$checked.'/>&nbsp;&nbsp;&nbsp;<i style="color:#aaa">'.$help.'</i></p>';
 						break;
 
 					case 'radio':
-						$edit = '<p style="margin: 5px">';
+						$edit = '<p style="margin: 2px">';
 						while(list($option_key, $option_name) = each($params['values']))
 							{
 							if($value == $option_key)  $checked = 'checked="checked"'; else $checked = '';
@@ -349,11 +348,13 @@ if(!class_exists('curlcast'))
 						break;
 
 					case 'text':
-						$edit = '<input id="'.htmlspecialchars($option).'" type="text" name="'.htmlspecialchars($option).'" value="'.htmlspecialchars($value).'" style="width:100%;"/><p style="margin:10px 0;color:#aaa"><i>'.$help.'</i></p>';
+						$edit = '<input id="'.htmlspecialchars($option).'" type="text" name="'.htmlspecialchars($option).'" value="'.htmlspecialchars($value).'" style="width:100%;"/>
+								<p style="margin:2px 0;color:#aaa"><i>'.$help.'</i></p>';
 						break;
 
 					case 'textarea':
-						$edit = '<textarea id="'.htmlspecialchars($option).'" name="'.htmlspecialchars($option).'" style="width:100%;height:70px;">'.htmlspecialchars(stripslashes($value)).'</textarea><p style="margin:10px 0;color:#aaa"><i>'.$help.'</i></p>';
+						$edit = '<textarea id="'.htmlspecialchars($option).'" name="'.htmlspecialchars($option).'" style="width:100%;height:70px;">'.htmlspecialchars(stripslashes($value)).'</textarea>
+								<p style="margin:2px 0;color:#aaa"><i>'.$help.'</i></p>';
 						break;
 
 					case 'dropdown_pages':
@@ -370,7 +371,7 @@ if(!class_exists('curlcast'))
 							'option_none_value' => ''
 							);
 						$edit = wp_dropdown_pages($args);
-						$edit .= '<i style="color:#aaa">'.$help.'</i>';
+						$edit .= '<br /><i style="color:#aaa">'.$help.'</i>';
 						break;
 					}
 
@@ -435,6 +436,14 @@ if(!class_exists('curlcast'))
     			echo '<div class="error fade"><p>'.sprintf(__('There\'s a new version of Curlcast available! You should <a href="%1$s">upgrade now</a>', 'curlcast'), WP_CURLCAST_URL_UPDATE).'</p></div>';
     			}
     		update_option('curlcast_lastcheck', time());
+			}
+
+		/**
+		 * Auto update
+		 */
+		function auto_update()
+			{
+			new curlcast_auto_update(WP_CURLCAST_VERSION, WP_CURLCAST_UPDATE_URL, plugin_basename(__FILE__));
 			}
 		}
 
@@ -515,6 +524,144 @@ if(!class_exists('curlcast'))
 				}
 			}
 
+		class curlcast_auto_update
+			{
+			/**
+			* The plugin current version
+			* @var string
+			*/
+			public $current_version;
+
+			/**
+			* The plugin remote update path
+			* @var string
+			*/
+			public $update_path;
+
+			/**
+			* Plugin Slug (plugin_directory/plugin_file.php)
+			* @var string
+			*/
+			public $plugin_slug;
+
+			/**
+			* Plugin name (plugin_file)
+			* @var string
+			*/
+			public $slug;
+
+			/**
+			* Initialize a new instance of the WordPress Auto-Update class
+			* @param string $current_version
+			* @param string $update_path
+			* @param string $plugin_slug
+			*/
+			function __construct($current_version, $update_path, $plugin_slug)
+				{
+				// Set the class public variables
+				$this->current_version = $current_version;
+				$this->update_path = $update_path;
+				$this->plugin_slug = $plugin_slug;
+				list ($t1, $t2) = explode('/', $plugin_slug);
+				$this->slug = str_replace('.php', '', $t2);
+
+				// define the alternative API for updating checking
+				add_filter('pre_set_site_transient_update_plugins', array(&$this, 'check_update'));
+
+				// Define the alternative response for information checking
+				add_filter('plugins_api', array(&$this, 'check_info'), 10, 3);
+				}
+
+			/**
+			* Add our self-hosted autoupdate plugin to the filter transient
+			*
+			* @param $transient
+			* @return object $ transient
+			*/
+			public function check_update($transient)
+				{
+				if (empty($transient->checked))
+					{
+					return $transient;
+					}
+
+				// Get the remote version
+				$remote_version = $this->getRemote_version();
+
+				// If a newer version is available, add the update
+				if (version_compare($this->current_version, $remote_version, '<'))
+					{
+					$obj = new stdClass();
+					$obj->slug = $this->slug;
+					$obj->new_version = $remote_version;
+					$obj->url = $this->update_path;
+					$obj->package = $this->update_path;
+					$transient->response[$this->plugin_slug] = $obj;
+					}
+				return $transient;
+				}
+
+			/**
+			* Add our self-hosted description to the filter
+			*
+			* @param boolean $false
+			* @param array $action
+			* @param object $arg
+			* @return bool|object
+			*/
+			public function check_info($false, $action, $arg)
+				{
+				if ($arg->slug === $this->slug)
+					{
+					$information = $this->getRemote_information();
+					return $information;
+					}
+				return false;
+				}
+
+			/**
+			* Return the remote version
+			* @return string $remote_version
+			*/
+			public function getRemote_version()
+				{
+				$request = wp_remote_post($this->update_path, array('body' => array('action' => 'version')));
+				if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200)
+					{
+					return $request['body'];
+					}
+				return false;
+				}
+
+			/**
+			* Get information about the remote version
+			* @return bool|object
+			*/
+			public function getRemote_information()
+				{
+				$request = wp_remote_post($this->update_path, array('body' => array('action' => 'info')));
+				if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200)
+					{
+					return unserialize($request['body']);
+					}
+				return false;
+				}
+
+			/**
+			* Return the status of the plugin licensing
+			* @return boolean $remote_license
+			*/
+			public function getRemote_license()
+				{
+				$request = wp_remote_post($this->update_path, array('body' => array('action' => 'license')));
+				if (!is_wp_error($request) || wp_remote_retrieve_response_code($request) === 200)
+					{
+					return $request['body'];
+					}
+				return false;
+				}
+			}
+
 
 	/**
 	Init plugin
@@ -527,4 +674,5 @@ if(!class_exists('curlcast'))
 
 	curlcast_main();
 	}
+
 ?>
