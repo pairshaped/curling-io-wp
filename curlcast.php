33dcd546134ad9e83a60419972594b0650bb293e
Alex <ulkoalex@gmail.com>
2014-08-13 23:34:02 +0300

    plugins url

--- curlcast.php
+++ curlcast.php
@@ -144,7 +144,7 @@ if (!class_exists('curlcast')) {
       add_menu_page('Curlcast Options', 'Curlcast', 8, __FILE__, array(
         'curlcast',
         'settings'
-      ), plugins_url('curlcast/settings.gif'));
+      ), plugins_url('settings.gif', __FILE__));
     }
 
     /**
