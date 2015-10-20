a17041d96e3ae095d6adab0673f333a38a1e6af0
Alex Barry <alex@pairshaped.ca>
2015-10-20 15:05:05 +0000

    Formatted the page with tables (gross\!), added some better formating via css, embedded a test component for the admin view to ensure settings are working.

--- admin/partials/curlcast-admin-display.php
+++ admin/partials/curlcast-admin-display.php
@@ -13,51 +13,89 @@
  */
 
 $curlcast_api = get_option($this->curlcast_setting_prefix . '_widgets_api', 'http://widgets.curlcast.ca');
-$curlcast_manifest_json = get_option($this->curlcast_setting_prefix . '_manifest_json', '{}');
 
 ?>
 
 <div class="wrap">
-  <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
-  <form action="options.php" method="POST">
-      <?php
-          settings_fields( $this->plugin_name );
-          do_settings_sections( $this->plugin_name );
-          submit_button();
-      ?>
-  </form>
-
-  <hr />
-
-  <h2>Summary</h2>
-  <table id="curlcast__summary" class='form-table' data-widgets-location="<?php echo "{$curlcast_api}" ?>">
+  <table>
     <tr>
-      <td colspan="2">
+      <td id="curlcast__settings">
+        <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+        <form action="options.php" method="POST">
+            <?php
+                settings_fields( $this->plugin_name );
+                do_settings_sections( $this->plugin_name );
+                submit_button();
+            ?>
+        </form>
+      </td>
+      <td id="curlcast__summary" data-widgets-location="<?php echo $curlcast_api; ?>">
+        <h2>Summary</h2>
         <h3>Short Codes</h3>
-      </td>
-    </tr>
-    <tr>
-      <td>Full Scoreboard</td>
-      <td><input type="text" readonly="readonly" length="24" value="[curlcast]" /></td>
-    </tr>
-    <tr>
-      <td colspan="2">
+        <p>Copy and Paste to embed full widget</p>
+
+        <h4>Full Scoreboard</h4>
+        <input type="text" readonly="readonly" length="24" value="[curlcast]" />
+
         <h3>Widgets Debug</h3>
+        <p>When reporting a bug, please copy and paste these values to us</p>
+        <h4>Full Entry Point</h4>
+        <span id="widgets-debug-full-entry"></span>
+        <h4>Mini Entry Point</h4>
+        <span id="widgets-debug-mini-entry"></span>
+        <h4>Icons Entry Point</h4>
+        <span id="widgets-debug-icons-entry"></span>
       </td>
-    </tr>
-    <tr>
-      <td>Full Entry Point</td>
-      <td id="widgets-debug-full-entry"></td>
-    </tr>
-    <tr>
-      <td>Mini Entry Point</td>
-      <td id="widgets-debug-mini-entry"></td>
-    </tr>
-    <tr>
-      <td>Icons Entry Point</td>
-      <td id="widgets-debug-icons-entry"></td>
     </tr>
   </table>
 
-</div>
+  <h3>Widget Sample</h3>
+  <table>
+    <tr>
+      <td width="75%">
+        <div class="test-container">
+          <div id='curlcastFull' class="curlcast" ></div>
 
+        </div>
+      </td>
+      <td>
+        TODO: Mini Widget
+      </td>
+    </tr>
+  </table>
+</div>
+<script type="text/javascript">
+  (function(ajaxGet){
+    'use strict';
+
+    var injectScript;
+
+    injectScript = function(scriptUrl) {
+      var scriptTag = document.createElement('script');
+      scriptTag.type = 'text/javascript';
+      scriptTag.src = scriptUrl;
+      document.body.appendChild(scriptTag);
+    };
+
+    addEventListener('DOMContentLoaded', function() {
+      ajaxGet("<?php echo $curlcast_api; ?>/manifest.json", {
+        success: function(response) {
+          var loadIndexes = [0, 1],
+              scriptIndex,
+              scriptFile,
+              index;
+          for(index=0; index < loadIndexes.length; index++) {
+            scriptIndex = loadIndexes[index]
+            scriptFile = response[scriptIndex]
+            injectScript("<?php echo $curlcast_api; ?>/" + scriptFile);
+          }
+        },
+        error: function(request) {
+          log('Something bad happened', request);
+        }
+      });
+    }, true);
+
+
+  })(ajaxGet);
+</script>
