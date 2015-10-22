ea35a68d63634d0ee250a3864dcef395526a71a6
Alex Barry <alex@pairshaped.ca>
2015-10-22 19:27:52 +0000

    Fixed up mixed spacing. Some parts had 2 and some had 4. PHP style guide specifies 4 as preferred.

--- public/class-curlcast-public.php
+++ public/class-curlcast-public.php
@@ -2,31 +2,31 @@
 
 class Curlcast_Public {
 
-  private $plugin_name;
+    private $plugin_name;
 
-  private $version;
+    private $version;
 
-  public function __construct( $plugin_name, $version ) {
+    public function __construct( $plugin_name, $version ) {
 
-    $this->plugin_name = $plugin_name;
-    $this->version = $version;
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
 
-  }
+    }
 
-  public function enqueue_styles() {
+    public function enqueue_styles() {
 
-    wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/curlcast-public.css', array(), $this->version, 'all' );
+        wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/curlcast-public.css', array(), $this->version, 'all' );
 
-  }
+    }
 
-  public function enqueue_scripts() {
+    public function enqueue_scripts() {
 
-    $plugins_root = plugin_dir_url(dirname(__FILE__));
+        $plugins_root = plugin_dir_url(dirname(__FILE__));
 
-    wp_enqueue_script( $this->plugin_name, $plugins_root . '/public/js/curlcast-public.js', array(), $this->version, false );
-    wp_enqueue_script( $this->plugin_name . '_common', $plugins_root . '/common/js/ajaxGet.js', array(), $this->version, false );
+        wp_enqueue_script( $this->plugin_name, $plugins_root . '/public/js/curlcast-public.js', array(), $this->version, false );
+        wp_enqueue_script( $this->plugin_name . '_common', $plugins_root . '/common/js/ajaxGet.js', array(), $this->version, false );
 
-  }
+    }
 
     public function add_shortcode( $args ) {
         include_once 'partials/curlcast-public-display.php';
