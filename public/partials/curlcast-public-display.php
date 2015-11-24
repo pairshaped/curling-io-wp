c84172840b6be71461cd0e2885830b8ef92f3dd6
Alex Barry <alex@pairshaped.ca>
2015-11-24 20:50:39 +0000

    Passed along language to the widget and full scoreboard.

--- public/partials/curlcast-public-display.php
+++ public/partials/curlcast-public-display.php
@@ -11,5 +11,6 @@ $div_id = "curlcast-full-{$uuid}";
 
 <div id='<?php echo $div_id; ?>' class="curlcast-v2 curlcast-full"></div>
 
+<script type="text/javascript">window.CurlcastWidgetsV2 = { LANGUAGE: "<?php echo $curlcast_language ?>" };</script>
 <script type="text/javascript">window.CurlcastV2 = window.CurlcastV2 || {mini: {}, full: {}}; CurlcastV2.full.widgets_api = "<?php echo $curlcast_widgets_api; ?>"; CurlcastV2.full.api_host = "<?php echo $curlcast_api_host; ?>"; CurlcastV2.full.api_key = "<?php echo $curlcast_api_key; ?>"; CurlcastV2.full.domTarget = "<?php echo $div_id; ?>";</script>
 
