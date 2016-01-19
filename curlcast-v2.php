dc3c77c1b2031377ae4d62858138c26cbf637ce4
Alex <alex.barry@gmail.com>
2016-01-19 14:03:39 -0500

    Bumped up version - not sure what happened last time.

--- curlcast-v2.php
+++ curlcast-v2.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       Curlcast V2 Wordpress Plugin
  * Description:       Curlcast Live Curling Statistics
- * Version:           2.1.1
+ * Version:           2.1.2
  * Author:            PairShaped
  * Author URI:        http://pairshaped.ca
  * Text Domain:       curlcast
