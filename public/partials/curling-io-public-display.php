6cb8bd1dfe6dd590b938d7d491a17b7db2f11b5a
Alex Barry <alex.barry@gmail.com>
2017-08-02 16:05:06 -0400

    Bumped to v3.0.3, fixed debug html comment with the version of the plugin.

--- public/partials/curling-io-public-display.php
+++ public/partials/curling-io-public-display.php
@@ -10,7 +10,7 @@ $base_url = get_page_link($page_id);
 $div_id = "curling-io-full";
 ?>
 
-<!-- curling.io v<? echo $plugin_version ?> -->
+<!-- curling.io v<?php echo $plugin_version ?> -->
 <div id='<?php echo $div_id; ?>' class="curling-io curling-io-full curlcast-v2 curlcast-full"></div>
 <script type="text/javascript">window.CurlcastWidgetsV2 = window.CurlcastWidgetsV2 || { LANGUAGE: "<?php echo $language ?>"}</script>
 <script type="text/javascript">window.CurlCastWidgets.mountFull({widgetsApi: "<?php echo $curling_io_widgets_api; ?>", apiHost: "<?php echo $curling_io_api_host; ?>", apiKey: "<?php echo $curling_io_api_key; ?>", baseUrl: "<?php echo $base_url; ?>"}, document.getElementById("<?php echo "$div_id"; ?>"));</script>
--- public/partials/curling-io-public-mini-display.php
+++ public/partials/curling-io-public-mini-display.php
@@ -10,7 +10,7 @@ $base_url = get_page_link($page_id);
 $div_id = "curling-io-mini";
 ?>
 
-<!-- curling.io v<? echo $plugin_version ?> -->
+<!-- curling.io v<?php echo $plugin_version ?> -->
 <div id='<?php echo $div_id; ?>' class="curling-io curling-io-mini curlcast-v2 curlcast-mini"></div>
 <script type="text/javascript">window.CurlcastWidgetsV2 = window.CurlcastWidgetsV2 || { LANGUAGE: "<?php echo $language ?>"}</script>
 <script type="text/javascript">window.CurlCastWidgets.mountMini({widgetsApi: "<?php echo $curling_io_widgets_api; ?>", apiHost: "<?php echo $curling_io_api_host; ?>", apiKey: "<?php echo $curling_io_api_key; ?>", baseUrl: "<?php echo $base_url; ?>"}, document.getElementById("<?php echo "$div_id"; ?>"));</script>
