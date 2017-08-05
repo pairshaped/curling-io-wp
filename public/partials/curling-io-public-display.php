9fde003c31ee29974560032a91d4f2e3520710d6
Alex Barry <alex.barry@gmail.com>
2017-08-05 11:13:05 -0400

    Searched and updated every last reference of doing <?php echo $variable ?> without a semicolon to with a semicolon.

--- public/partials/curling-io-public-display.php
+++ public/partials/curling-io-public-display.php
@@ -12,5 +12,5 @@ $div_id = "curling-io-full";
 
 <!-- curling.io v<?php echo $plugin_version; ?> -->
 <div id='<?php echo $div_id; ?>' class="curling-io curling-io-full curlcast-v2 curlcast-full"></div>
-<script type="text/javascript">window.CurlcastWidgetsV2 = window.CurlcastWidgetsV2 || { LANGUAGE: "<?php echo $language ?>"}</script>
+<script type="text/javascript">window.CurlcastWidgetsV2 = window.CurlcastWidgetsV2 || { LANGUAGE: "<?php echo $language; ?>"}</script>
 <script type="text/javascript">window.CurlCastWidgets.mountFull({widgetsApi: "<?php echo $curling_io_widgets_api; ?>", apiHost: "<?php echo $curling_io_api_host; ?>", apiKey: "<?php echo $curling_io_api_key; ?>", baseUrl: "<?php echo $base_url; ?>"}, document.getElementById("<?php echo "$div_id"; ?>"));</script>
