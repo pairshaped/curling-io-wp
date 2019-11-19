09c913f25b5b332278ee9f974edce194237e380d
Dave Rapin <dave@rapin.com>
2019-11-18 19:46:49 -0500

    Changed the API URL to go through fastly

--- includes/constants.php
+++ includes/constants.php
@@ -1,6 +1,6 @@
 <?php
 
 define('CURLING_IO_DEFAULT_WIDGETS', 'https://widgets.curling.io');
-define('CURLING_IO_DEFAULT_API', 'https://legacy.curling.io');
+define('CURLING_IO_DEFAULT_API', 'https://legacy-curlingio.global.ssl.fastly.net');
 define('CURLING_IO_SHORTCODE', 'curling_io_full');
 define('CURLING_IO_SHORTCODE_LEGACY', 'curlcast_full');
